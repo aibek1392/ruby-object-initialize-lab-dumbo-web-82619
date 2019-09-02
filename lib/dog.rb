109c5c9445573313dbd67f86a602d3c25a90c485
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-02 20:06:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 class Dog
  attr_writer :name
-  # def initialize(name)
-  #   @name = name
-  # end
+  def initialize(name)
+    @name = name
+  end
   # def name=(name)
   #   @name = name
   # end
